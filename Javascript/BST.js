d254f6c8587d8910a81e7e684d0925fb9819c667
Cody <codywalenciak@gmail.com>
2018-07-31 13:12:44 -0500

    traversals added

--- Javascript/BST.js
+++ Javascript/BST.js
@@ -43,9 +43,43 @@ function BST(){
             return this;
         }
     }
+
+    this.pre_traverse = function(root){
+        console.log(root.val)
+        if(root.left){
+            this.pre_traverse(root.left);
+        }
+        if(root.right){
+            this.pre_traverse(root.right);
+        }
+    }
+
+    this.post_traverse = function(root){
+        
+        if(root.left){
+            this.post_traverse(root.left);
+        }
+        console.log(root.val)
+        if(root.right){
+            this.post_traverse(root.right);
+        }
+    }
+
+    this.in_order_traverse = function(root){
+        if(root.left){
+            this.in_order_traverse(root.left);
+        }
+        
+        if(root.right){
+            this.in_order_traverse(root.right);
+        }
+        console.log(root.val)
+    }
 }
 
 
 newBST = new BST;
 
 console.log(newBST.insert(40).insert(25).insert(70).insert(50).insert(16).insert(90).insert(150).insert(20).insert(45).insert(75).insert(30))
+
+console.log(newBST.in_order_traverse(newBST.root))
