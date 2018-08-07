345467c4775c818a8cd78542aceaeac0cb7eb4b4
Cody <codywalenciak@gmail.com>
2018-08-07 13:06:48 -0500

    ~~Mongoose~~ Owl Dashboard

---
+++ mongo/mongoose_dashboard/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const flash = require('express-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const PORT = 1337;
+
+app.use(session({
+    secret:'notasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 60000}
+}))
+app.use(flash());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "./public")));
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+require("./server/config/mongoose");
+require("./server/config/routes")(app);
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port: ${PORT}!`);
+})
