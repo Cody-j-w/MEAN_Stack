a056ca66ac2787c8c32b5a9beac75f4813499134
Cody <codywalenciak@gmail.com>
2018-08-09 18:29:32 -0500

    It begins - Angular.

---
+++ Angular/hello_angular/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 1337;
+
+app.use(bodyParser.json());
+app.use(express.static( __dirname + '/public/dist/public' ));
+
+
+//require("./server/config/mongoose");
+//require("./server/config/routes")(app);
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port: ${PORT}!`);
+})
